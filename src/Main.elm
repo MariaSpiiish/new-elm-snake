dc1e73db17f233d16c1771fe894828a41ee9aa1f
Maria Lapshina <mariaspiiish@gmail.com>
2023-08-18 09:45:28 +0400

    added a List of Lists Model, able to compile with no errors

---
+++ src/Main.elm
@@ -0,0 +1,41 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, div)
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model = List (List ())
+
+init : Model
+init =
+  [[],[],[]]
+
+
+-- UPDATE
+
+
+type Msg
+  = Model
+
+
+update : Msg -> Model -> Model
+update msg model = model
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div [] []
